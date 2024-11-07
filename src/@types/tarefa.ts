64afdaeb0291beb978561b5b3e9943415cf5bb2f
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-11-07 09:22:54 -0300

    raw: adicionando tipo para item de tarefa

--- src/@types/tarefa.ts
+++ src/@types/tarefa.ts
@@ -3,3 +3,11 @@ export interface ITarefa {
   custo: number;
   dataLimite: Date;
 }
+
+export interface ITarefaItem {
+  id: number;
+  nome: string;
+  custo: number;
+  dataLimite: Date;
+  ordemApresentacao: number;
+}
