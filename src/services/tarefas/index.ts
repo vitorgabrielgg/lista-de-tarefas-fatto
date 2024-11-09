c210eb1881a1efd23182b103aa32345a28a1f811
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-11-09 10:01:06 -0300

    feat: serviço para criar tarefa

--- src/services/tarefas/index.ts
+++ src/services/tarefas/index.ts
@@ -1,6 +1,21 @@
+import { ITarefa } from "@/@types";
+
 export class TarefasRequestService {
   static async getTarefas() {
     const res = await fetch("api/tarefas");
+    const data = await res.json();
+
+    return data;
+  }
+
+  static async postTarefa(params: ITarefa) {
+    const res = await fetch("api/tarefas", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(params),
+    });
     const data = await res.json();
 
     return data;
